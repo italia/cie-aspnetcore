97452d102daeef55ef19a41da96537780fb01c5d
Daniele Giallonardo <daniele.giallonardo@asfweb.it>
2021-02-04 16:47:55 +0100

    Aggiunto flag Secure a cookie

--- CIE.AspNetCore.Authentication/CIE.AspNetCore.Authentication/CieHandler.cs
+++ CIE.AspNetCore.Authentication/CIE.AspNetCore.Authentication/CieHandler.cs
@@ -589,7 +589,11 @@ namespace CIE.AspNetCore.Authentication
 
         public static void Save(this AuthenticationProperties properties, HttpResponse response, ISecureDataFormat<AuthenticationProperties> encryptor)
         {
-            response.Cookies.Append(CieDefaults.CookieName, encryptor.Protect(properties), new CookieOptions() { SameSite = SameSiteMode.None });
+            response.Cookies.Append(CieDefaults.CookieName, encryptor.Protect(properties), new CookieOptions()
+            {
+                SameSite = SameSiteMode.None,
+                Secure = true
+            });
         }
 
         public static void Load(this AuthenticationProperties properties, HttpRequest request, ISecureDataFormat<AuthenticationProperties> encryptor)
